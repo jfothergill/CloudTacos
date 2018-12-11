fb636d77a340ba77e397b688a55a7bc20c6c986f
Jeremy Fothergill <jeremy.fothergill@gmail.com>
2018-12-11 12:38:12 +0000

    Initial commit of Cloud Tacos Project

---
+++ src/main/java/com/jfothergill/cloudtacos/CloudTacosApplication.java
@@ -0,0 +1,12 @@
+package com.jfothergill.cloudtacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTacosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jfothergill/cloudtacos/CloudTacosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jfothergill.cloudtacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudTacosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
